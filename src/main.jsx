40f2d408d0b1e078044471c77d30d46d2737b6ec
dolodorsey <dolodorsey@gmail.com>
2026-08-19 01:27:05 -0400

    Enforce Help 911 provider privacy in public app

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import Help911App from "./App.jsx";
 import RequestHelp from "./RequestHelp.jsx";
 import { initNative } from "./native.js";
+import { initProviderPrivacy } from "./providerPrivacy.js";
 import { getRouteComponent } from "./routes.jsx";
 
 initNative();
+initProviderPrivacy();
 
 // Path-aware shell. Focused public routes render their own landing/intake while
 // the main HELP 911 application remains the default experience.
